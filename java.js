fcee1acbbd77094992740ea4b187c9173610dc1f
Pablo Teixeira <101361880+pabllot@users.noreply.github.com>
2022-03-28 20:17:59 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,79 @@
+const API_KEY = 'api_key=831770edb6c80f00597be28853c57095';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500'
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const header = document.getElementById('header')
+
+getMovies(API_URL);
+
+function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML='';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        
+            <img src="${IMG_URL+poster_path}" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">${overview}       
+            </div>
+        </div>
+
+        
+        `
+
+        main.appendChild(movieEl);
+    
+    })
+}
+
+function getColor(vote){
+    if(vote>=8){
+        return 'green'
+    } else if (vote>=5){
+        return 'orange'
+    } else {
+    return 'red'}
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    } else {
+        getMovies(API_URL);
+    }
+
+})
+
+const button = document.getElementById('button')
+
+const doit = function doit(){
+    main.classList.toggle('dark')   
+
+}
+
+button.addEventListener('click', doit)
